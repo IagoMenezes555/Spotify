42a6dc9dd3408e94460f19b17dcebcfff2495ac0
Iago Menezes <iagomenezes555@gmail.com>
2025-07-07 16:31:17 -0300

    add: card

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../card/card.component';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
