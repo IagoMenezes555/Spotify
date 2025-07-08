3840a0552088672eacd395bbeb1145304dc8ee51
Iago Menezes <iagomenezes555@gmail.com>
2025-07-08 14:28:33 -0300

    add: carousel

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
+import { MusicsService } from '../../services/musics.service';
 
 @Component({
   selector: 'app-main',
-  imports: [CardComponent],
+  imports: [CardComponent, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  musicas: Musicas;
+  @ViewChild('carousel', { static: false }) carousel!: ElementRef;
 
+  constructor(private musicsService: MusicsService){
+    this.musicas = new Musicas(this.musicsService);
+  }
+
+  musics = [{}]
+
+  scrollLeft() {
+    this.carousel.nativeElement.scrollBy({
+      left: -300,
+      behavior: 'smooth'
+    });
+  }
+
+  scrollRight() {
+    this.carousel.nativeElement.scrollBy({
+      left: 300,
+      behavior: 'smooth'
+    });
+  }
+
+  ngOnInit(){
+    this.musicas.enviarMusicas();
+    this.musics = this.musicsService.musics;
+  }
+}
+
+export class Musicas {
+  constructor(public musicsService: MusicsService){}
+
+  enviarMusicas(){
+
+    let musics = [
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+    {nome: "teste", cantor: "teste"},
+  ]
+
+  this.musicsService.musics = musics;
+  }
 }
